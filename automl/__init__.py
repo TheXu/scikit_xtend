d0483ba87695fbc71819ad3c0b58de7e5295d28c
Alex Xu <ayx2@case.edu>
2019-02-15 07:03:43 -0700

    Added hyperparameter optimization module

---
+++ automl/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+scikit-xtend Machine Learning Library Extensions
+Created on 2019
+
+@author: Alex Xu <ayx2@case.edu>
+"""
+from .hyperparam_opt import EstimatorSelectionHelper
+
+__all__ = [
+    'EstimatorSelectionHelper'
+]
